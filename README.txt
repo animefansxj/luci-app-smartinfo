c275cf2b8f28047be86ddb2f03f910423f31cc91
animefansxj <af_xj@hotmail.com>
2016-02-13 21:01:54 +0800

    VER_0200

---
+++ README.txt
@@ -0,0 +1,41 @@
+﻿luci-app-smartinfo
+=================================================
+VERSION 2.0
+
+简述:
+    此脚本程序包含用于1个用于后台监控并记录硬盘健
+康状况的脚本，1个用户向LuCI汇报健康状况的脚本以及
+一个共用函数脚本。除去脚本外还包含一个LuCI设置页
+面以及一个LuCI状态页显示组件。
+    此工具用于在基于OpenWRT的路由器上监控USB硬盘
+盒中硬盘的监控状况而使用。
+
+
+测试环境:
+    在 OpenWRT 14.07 上开发
+    在 OpenWRT 15.05 上测试通过
+
+
+依赖关系:
+    smartmontools
+
+
+作者:
+    穿越蓝天 (animefans_xj / 穿越蓝天)
+    你可以在这里找到我:
+        数码之家 bbs.mydigit.cn
+        恩山论坛 www.right.com.cn
+    也可以通过电邮反馈:
+        af_xj@hotmail.cn
+
+
+授权:
+    此程序的当前版本采用GNU GPL V3协议授权，请务必
+保持分支版本的代码公开。
+    你可以在 http://www.gnu.org/licenses/licenses.html
+取得授权协议的副本。
+
+
+发布:
+    除上述站点外，所有自愿也通过github以及以下站点发布:
+      http://pan.baidu.com/s/1bnw7hCj#path=%252FSoftware%252FRouter
---
+++ luci-dir/applications/luci-app-smartinfo/luasrc/controller/smartinfo.lua
@@ -0,0 +1,70 @@
+--[[
+LuCI - Lua Configuration Interface - smartctl support
+
+Script by animefans_xj @ nvacg.org (af_xj@hotmail.com , xujun@smm.cn)
+
+Licensed under the GNU GPL License, Version 3 (the "license");
+you may not use this file except in compliance with the License.
+you may obtain a copy of the License at
+
+	http://www.gnu.org/licenses/gpl.txt
+
+$Id$
+]]--
+
+
+module("luci.controller.smartinfo",package.seeall)
+
+function index()
+	require("luci.i18n")
+	luci.i18n.loadc("smartinfo")
+	if not nixio.fs.access("/etc/config/smartinfo") then
+		return
+	end
+	
+	local page = entry({"admin","services","smartinfo"},cbi("smartinfo"),_("S.M.A.R.T Info"))
+	page.i18n="smartinfo"
+	page.dependent=true
+
+	entry({"admin","services","smartinfo","status"}, call("smart_status")).leaf = true
+end
+
+
+function smart_status()
+  local cmd = io.popen("/usr/lib/smartinfo/smart_status.sh")
+  if cmd then
+    local dev = { }
+    while true do
+      local ln = cmd:read("*l")
+      if not ln then
+        break
+      elseif ln:match("^.+:.+") then
+        local name,status = ln:match("^.+/(.+):(.+)")
+        local model,size
+        
+        if (status=="OK" or status=="Failed" or status=="Unsupported") then
+          model="%s %s" % {nixio.fs.readfile("/sys/class/block/%s/device/vendor" % name), nixio.fs.readfile("/sys/class/block/%s/device/model" % name)}
+          local s = tonumber((nixio.fs.readfile("/sys/class/block/%s/size" % name)))
+          size = "%s MB" % {s and math.floor(s / 2048)}
+        else
+          model="Unavailabled"
+          size="Unavailabled"
+        end
+        
+        if name and status then
+            dev[#dev+1]= {
+              name = name,
+              model = model,
+              size  = size,
+              status  = status
+            }
+        end
+      end
+    end
+  
+  cmd:close()
+  luci.http.prepare_content("application/json")
+  luci.http.write_json(dev)
+  end
+end
+
